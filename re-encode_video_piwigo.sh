9e181a78046034451cefdccf6dd4c351df54f3e0
emptythevoid <emptythevoid@gmail.com>
2020-01-31 17:55:21 -0500

    Add files via upload

--- re-encode_video_piwigo.sh
+++ re-encode_video_piwigo.sh
@@ -2,6 +2,9 @@
 
 # Takes all .mp4 files in current directory and re-encodes them optimized for PiWiGo
 
+# Requires
+# handbrakecli
+
 for file in *.mp4; do
 
     HandBrakeCLI --optimize --encoder x264 --x264-profile high -i $file -o "convert"_$file
